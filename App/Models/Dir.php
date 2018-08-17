6f118beee12dc8847ea257245547db6cc0ba4e97
Luka Kruljac <luka97kruljac@gmail.com>
2018-08-17 15:37:27 +0200

    petak

--- App/Models/Dir.php
+++ App/Models/Dir.php
@@ -17,11 +17,48 @@ class Dir extends \Core\Model
     public static function getAcc($username){
         try {
             $db = static::getDB();
-            $results = $db->query("SELECT 'id', 'name', 'username', 'password' FROM users WHERE username='$username' ");
+            $stmt = $db->query("SELECT id, name, username, email FROM users WHERE username='$username' ");
+
+            $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
             return $results;
+        }catch(DOException $e){
+
+        }
+        return 0;
+    }
+
+    public static function insertFile($username, $path, $filename){
+        try {
+            $db = static::getDB();
+            $stmt = $db->query("SELECT id FROM users WHERE username='$username' ");
+            $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            $ownerid=$results[0]['id'];
+
+            $db->query("INSERT INTO  files (name, ownerid, path) VALUES('$filename', '$ownerid', '$path')");
+
         }catch(DOException $e){
 
         }
 
     }
+    public static function getMyFiles($username){
+        try {
+            $db = static::getDB();
+            $stmt = $db->query("SELECT id FROM users WHERE username='$username' ");
+            $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            $ownerid=$results[0]['id'];
+
+            $db = static::getDB();
+            $stmt = $db->query("SELECT name, path FROM files WHERE ownerid='$ownerid' ");
+            $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            var_dump($results);
+            return $results;
+
+        }catch(DOException $e){
+
+        }
+        return $results;
+    }
+
+
 }
