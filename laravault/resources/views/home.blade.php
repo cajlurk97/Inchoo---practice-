49afd6563075d4f82f50e048cecec817952fb28f
Luka Kruljac <luka97kruljac@gmail.com>
2018-08-22 15:31:37 +0200

    edited butons

--- laravault/resources/views/home.blade.php
+++ laravault/resources/views/home.blade.php
@@ -12,15 +12,27 @@
         @guest
 
             <div class="row">
+
                 <div class="col-lg-12 text-center">
+
                     <h1 class="mt-5">Welcome</h1>
+
                     <p>Plesae, log in</p>
-                    <a href="/login" class="button class btn-primary">Login</a>
-                    <br>
-                    <br>
-                    <p>Or if you are new here</p>
-                    <a href=/register" class="button class btn-primary">Register</a>
+                        <div class="col-md-8 offset-md-4">
+                            <a class="btn btn-primary" href="{{ route('login') }}">
+                                {{ __('Login') }}
+                            </a>
+                        </div>
+
+                    <p><br>Have no acc?</p>
+                        <div class="col-md-8 offset-md-4">
+                            <a class="btn btn-primary" href="{{ route('register') }}">
+                                {{ __('Register') }}
+                            </a>
+                        </div>
+
                 </div>
+
             </div>
 
 
@@ -28,8 +40,14 @@
 
             <div class="row">
                 <div class="col-lg-12 text-center">
+
                     <h1 class="mt-5">Welcome, {{Auth::user()->name}}</h1>
-                    <button class="button class btn-primary">MyFiles</button>
+
+                    <div class="col-md-8 offset-md-4">
+                        <a class="btn btn-primary" href="{{ route('login') }}">
+                            {{ __('MyFiles') }}
+                        </a>
+                    </div>
                 </div>
             </div>
 
